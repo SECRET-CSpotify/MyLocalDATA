62d43f3c2b30484a04ac3de401f39ee241e92d89
SECRET-CSpotify <juancbonillaf@gmail.com>
2025-08-27 21:28:52 -0500

    Update MyLocalDATA.py

--- MyLocalDATA.py
+++ MyLocalDATA.py
@@ -7,11 +7,18 @@ from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
 from db import crear_tabla, agregar_cliente, obtener_clientes, actualizar_cliente_detalle
 
-# --- Cargar credenciales desde secrets ---
-credentials = {
-    "usernames": dict(st.secrets["credentials"]["usernames"])
-}
+import copy
+def normalize_credentials_from_secrets():
+    creds = st.secrets.get("credentials")
+    if creds:
+        return {"usernames": copy.deepcopy(dict(creds["usernames"]))}
+    return None
 
+credentials = normalize_credentials_from_secrets()
+if credentials is None:
+    st.error("No se detectaron credenciales válidas en st.secrets.")
+    st.stop()
+    
 # --------------------------
 # Configuración general
 # --------------------------
